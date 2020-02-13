ab581e54e7025058b16ed03ec2885349367a49ed
Yuhua_Ding <yuhua.ding@columbia.edu>
2020-02-12 19:02:04 -0500

    Added a new line

---
+++ testrepo/src/testfile.R
@@ -0,0 +1,28 @@
+
+
+### this is a test file
+
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+print("Hello World!")
+print("Hello World!")
+print(":)")
+print("boom")
+print("this is Chengwei speaking:")
+
+#Let's create some vectors
+V1 <- rnorm(20, mean = 0, sd = 1)
+V2 <- rnorm(20, mean = -1, sd = 0.5)
+V3 <- runif(20, min = 0, max = 1)
+
+
+
+#Let's create some vectors
+V1 <- rnorm(20, mean = 0, sd = 1)
+V2 <- rnorm(20, mean = -1, sd = 0.5)
+V3 <- runif(20, min = 0, max = 1)
+
+
+print("2/12/2020")
