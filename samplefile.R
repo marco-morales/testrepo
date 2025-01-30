4d553546391a83c3d61167ad6d1ec5c85757b1cf
Marco Morales <marco.morales@nyu.edu>
2025-01-29 19:22:38 -0500

    Commit initial files

---
+++ samplefile.R
@@ -0,0 +1,33 @@
+
+# THIS IS A COMMENT
+
+
+a <- 7
+
+b <- "test"
+
+
+# vectors 
+
+
+v <- c(1, 2, 3)
+
+length(v)
+
+c(v, v)
+
+
+v[3] <- 25
+
+number_sequence <- 1:10
+number_sequence[3:5]
+
+sum(number_sequence)
+
+install.packages("tidyverse")
+
+
+library(tidyverse)
+
+print("Hello World!")
+
